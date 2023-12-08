85583a3decf310f113d6e1d9c48241a2d1eaccd7
AdamLoginov <mysnikov2000@mail.ru>
2023-12-08 18:18:18 +0300

    change 6

--- command.sh
+++ command.sh
@@ -2,6 +2,7 @@ sudo apt-get update -y
 sudo apt-get install -y screen
 sudo apt-get install -y python3-pip
 pip3 install selenium
+pip3 install requests
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
